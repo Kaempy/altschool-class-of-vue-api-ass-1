a268348d725bb5d825b3203e5cf92f47a3dba02e
Kaempy <campa.banj@gmail.com>
2023-02-09 12:15:43 +0100

    converted to composables

---
+++ src/components/ColorPickerGame.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Color Picker Game</h1>
+
+  <div>{{ message }}</div>
+
+  <button v-for="color in colors" :key="color" @click="matchColor(color)">
+    {{ color }}
+  </button>
+</template>
+
+<script>
+// import { ref } from "vue";
+import ColorPicker from "../composables/ColorPicker";
+
+export default {
+  setup() {
+    const { colors, message, matchColor } = ColorPicker();
+
+    return { colors, message, matchColor };
+  },
+};
+</script>
+
+<style></style>
